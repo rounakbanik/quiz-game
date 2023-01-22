9b650e420c6850c2dd8c2f8b65ad9921157af54d
Rounak Banik <rounakbanik@gmail.com>
2023-01-22 18:28:54 +0530

    Add v1 version of game

--- data/constants.js
+++ data/constants.js
@@ -1,6 +1,11 @@
 const goerliRpc = "https://TRTFSPZ6KTFXYZ2Q2E7A:SJNBSXEKAIZJJVZDMVHB4BOAJHHABTGEHNSXLLHN@goerli.ethereum.coinbasecloud.net";
+
+const username = "W4SSZX6JDPG3UWIZVESM";
+const password = "FH7X6MGJ2JJLXLUYUZVQQ35CGMBQDYWLMNCH45VJ";
+
 const mainnetRpc = "https://W4SSZX6JDPG3UWIZVESM:FH7X6MGJ2JJLXLUYUZVQQ35CGMBQDYWLMNCH45VJ@mainnet.ethereum.coinbasecloud.net";
+
 const ownerAddress = "0xD9942E62f0dE0A339ac86Ee0EC758F6e55Ae41A8";
 const contractAddress = "0x524Dd04b9cEBE310218272D325c8Aae72D246054";
 
-export { goerliRpc, mainnetRpc, ownerAddress, contractAddress }
+export { goerliRpc, mainnetRpc, username, password, ownerAddress, contractAddress }
